1670c9b7cf71cb28fb6222a3e129afd4ed95a0ad
Allan Hillyer <hillyeal@oregonstate.edu>
2021-03-07 20:09:42 -0800

    wrote implementation in checkLeap() {div/4 = leapyear}

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,7 @@
 def checkLeap(number):
-    if(number%400 == 0):
+    if(number%4 == 0):
+        if(number%400 == 0):
+            return "leapyear"
+        if(number%100 == 0):
+            return "not leapyear"
         return "leapyear"
-    if(number%100 == 0):
-        return "not leapyear"
