db19611a635e620e411795cea06958ee57740187
Allan Hillyer <hillyeal@oregonstate.edu>
2021-03-07 19:51:51 -0800

    wrote second test {check that num/100 but not 400 returns 'not leapyear'}

--- test_leapyear.py
+++ test_leapyear.py
@@ -2,11 +2,16 @@ import unittest
 import leapyear
 
 class TestCase(unittest.TestCase):
-    #test to check if numbers divisible by 400 return leapyear
+    #test to check if numbers divisible by 400 return "leapyear"
     def test_fourhundred(self):
         hundreds = [0, 400, 800, 1200, 1600, 2000, 2400]
         for x in hundreds:
             self.assertEqual(leapyear.checkLeap(x), "leapyear")
     
+    #test to check that div/100 but not 400 returns "not leapyear"
+    def test_hundreds(self):
+        oneHundreds = [100, 200, 300, 500, 600, 700, 900, 1100, 1300, 1400, 1500, 1700]
+        for x in oneHundreds:
+            self.assertEqual(leapyear.checkLeap(x), "not leapyear")
 if __name__ == '__main__':
     unittest.main(verbosity=2)
